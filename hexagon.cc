6bb00e8d2e06c7e46616e1ed1fdf70f742df15b7
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-03 14:20:12 -0600

    Add hexagon from a1
    Overloaded drawHexagon for single color filled hexagon

---
+++ hexagon.cc
@@ -0,0 +1,43 @@
+#include "hexagon.hh"
+
+#include "colorSetter.hh"
+
+void drawHexagon(GLint x, GLint y, GLint w, GLint h, GLint delta,
+                 std::array<ColorSetter, 6> colors) {
+  // the x value that the diagonals meet up at
+  // on the left and right sides of the hexagon
+  GLint meetup = y - (h / 2);
+
+  // draw the diagonals
+  drawLine(x, meetup, x + delta, y - h, colors[0]); 
+  drawLine(x, meetup, x + delta, y, colors[1]);
+  drawLine(x + w - delta, y - h, x + w, meetup, colors[2]);
+  drawLine(x + w - delta, y, x + w, meetup, colors[3]);
+
+  // draw the horizontals
+  drawLine(x + delta, y - h, x + w - delta, y - h, colors[4]);
+  drawLine(x + delta, y, x + w - delta, y, colors[5]);
+}
+
+void drawHexagon(GLint x, GLint y, GLint w, GLint h, GLint delta,
+                 ColorSetter color) {
+  GLint meetup = y - (h / 2);
+
+  color();
+  glBegin(GL_POLYGON);
+  glVertex2i(x, meetup);
+  glVertex2i(x + delta, y - h);
+  glVertex2i(x + w - delta, y - h);
+  glVertex2i(x + w, meetup);
+  glVertex2i(x + w - delta, y);
+  glVertex2i(x + delta, y);
+  glEnd();
+}
+
+void drawLine(GLint x1, GLint y1, GLint x2, GLint y2, ColorSetter color) {
+  color(); // set the color by calling the function object
+  glBegin(GL_LINES);
+  glVertex2i(x1, y1); // Specify line segment geometry
+  glVertex2i(x2, y2);
+  glEnd();
+}
---
+++ hexagon.hh
@@ -0,0 +1,22 @@
+#pragma once  
+
+#include <array>
+#include <GL/glut.h>
+
+#include "colorSetter.hh"
+
+// draw a colored w by h octogon with x, y coordinates being the top left
+// corner of an imaginary encompassing square
+// delta is the distance from any vertex to the nearest corner of an
+// imaginary encompassing square
+// colors is an array of ColorSetters corresponding to each edge
+void drawHexagon(GLint x, GLint y, GLint w, GLint h, GLint delta,
+                 std::array<ColorSetter, 6> colors);
+
+// Same as above, except only one color is specified for all the edges
+// plus the fill color
+void drawHexagon(GLint x, GLint y, GLint w, GLint h, GLint delta,
+                 ColorSetter color);
+
+// draw a colored line starting at (x1, y1) and ending at (x2, y2) 
+void drawLine(GLint x1, GLint y1, GLint x2, GLint y2, ColorSetter color);
