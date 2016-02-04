6e2293b900e67270113ee7c2340a390906c0ccf5
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-03 19:03:37 -0600

    Refactor to use a2Object

--- main.cc
+++ main.cc
@@ -1,41 +1,17 @@
 #include <GL/glut.h>
 
-#include "colorSetter.hh"
-#include "hexagon.hh"
+#include "a2Object.hh"
 
-static GLfloat spin = 0.0; // how much the polygon on screen should spin
-static bool reverse = false; // whether spin velocity should be reversed
-static bool firstClick = true; // whether the next click is the first click
-static int yHome = 0; // the x position of the first mouse click
-static const GLfloat spinVelocityBase = 5.0;
-static GLfloat spinVelocity = spinVelocityBase;
-static GLfloat spinVelocityBias = 30.0; // spin velocity's resistance to change
+A2Object a2Object(-25, 25);
 
 void display(void)
 {
   glClear(GL_COLOR_BUFFER_BIT);
   glPushMatrix();
-  glRotatef(spin, 0.0, 0.0, 1.0);
-  ColorSetter color(1.0, 1.0, 1.0);
-  drawHexagon(-25, 25, 50, 50, 12, color);
+  a2Object.draw();
   glPopMatrix();
 
   glutSwapBuffers();
-}
-
-void spinDisplay(void)
-{
-  spin = spin + spinVelocity * (reverse ? -1 : 1);
-  if (spin > 360.0)
-    spin = spin - 360.0;
-  glutPostRedisplay();
-}
-
-void updateSpinVelocity(int y) {
-  GLfloat spinVelocityChange = (y - yHome) / spinVelocityBias;
-  spinVelocity = spinVelocityBase + spinVelocityChange;
-  if ((reverse && spinVelocity > 0.0) || spinVelocity < 0.0)
-    spinVelocity = 0.0;
 }
 
 void init(void) 
@@ -54,39 +30,15 @@ void reshape(int w, int h)
   glLoadIdentity();
 }
 
-void mouse(int button, int state, int x, int y) 
-{
-  switch (button) {
-  case GLUT_LEFT_BUTTON:
-    if (state == GLUT_DOWN) {
-      if (firstClick)
-        yHome = y;
-      firstClick = false;
-      reverse = false;
-      glutIdleFunc(spinDisplay);
-    }
-    else
-      glutIdleFunc(NULL);
-    break;
-  case GLUT_MIDDLE_BUTTON:
-    break;
-  case GLUT_RIGHT_BUTTON:
-    if (state == GLUT_DOWN) {
-      if (firstClick)
-        yHome = y;
-      firstClick = false;
-      reverse = true;
-      glutIdleFunc(spinDisplay);
-    }
-    else
-      glutIdleFunc(NULL);
-    break;
-  default:
-    break;
-   }
-  updateSpinVelocity(y);
+void idle() {
+  a2Object.update();
+  glutPostRedisplay();
 }
-   
+
+void mouse(int button, int status, int x, int y) {
+  a2Object.mouseEvent(button, status, x, y);
+}
+
 /* 
  *  Request double buffer display mode.
  *  Register mouse input callback functions
@@ -102,6 +54,7 @@ int main(int argc, char** argv)
   glutDisplayFunc(display); 
   glutReshapeFunc(reshape); 
   glutMouseFunc(mouse);
+  glutIdleFunc(idle);
   glutMainLoop();
   return 0;   /* ANSI C requires main to return int. */
 }
