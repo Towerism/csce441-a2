1ca95700cdebf869a122c12d6e5c620190c2dad5
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-04 11:36:49 -0600

    Refactor main.cc

--- main.cc
+++ main.cc
@@ -6,23 +6,30 @@
 
 Delegator delegator;
 
-void draw(void)
-{
-  glClear(GL_COLOR_BUFFER_BIT);
-  delegator.draw();
-
-  glutSwapBuffers();
+void initGlut() {
+  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize (250, 250);
+  glutInitWindowPosition (100, 100);
+  glutCreateWindow ("Martin Fracker - Assignment 2");
 }
 
-void init(void) 
-{
+void initDisplay() {
   glClearColor (0.0, 0.0, 0.0, 0.0);
   glShadeModel (GL_FLAT);
+}
 
+void initEntities() {
   Background* background = new Background(-50, -50, 100, 100);
   A2Object* a2Object = new A2Object(-25, 25);
   delegator.add(background);
   delegator.add(a2Object);
+}
+
+void init(void) 
+{
+  initGlut();
+  initDisplay();
+  initEntities();
 }
 
 void reshape(int w, int h)
@@ -33,6 +40,14 @@ void reshape(int w, int h)
   glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
+}
+
+void draw(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  delegator.draw();
+
+  glutSwapBuffers();
 }
 
 void idle() {
@@ -48,6 +63,14 @@ void keyboard(unsigned char key, int x, int y) {
   delegator.keyboard(key,  x, y);
 }
 
+void attachCallbacks() {
+  glutDisplayFunc(draw); 
+  glutReshapeFunc(reshape); 
+  glutMouseFunc(mouse);
+  glutKeyboardFunc(keyboard);
+  glutIdleFunc(idle);
+}
+
 /* 
  *  Request double buffer delegator mode.
  *  Register mouse input callback functions
@@ -55,16 +78,8 @@ void keyboard(unsigned char key, int x, int y) {
 int main(int argc, char** argv)
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-  glutInitWindowSize (250, 250); 
-  glutInitWindowPosition (100, 100);
-  glutCreateWindow ("Martin Fracker - Assignment 2");
   init();
-  glutDisplayFunc(draw); 
-  glutReshapeFunc(reshape); 
-  glutMouseFunc(mouse);
-  glutKeyboardFunc(keyboard);
-  glutIdleFunc(idle);
+  attachCallbacks();
   glutMainLoop();
   return 0;   /* ANSI C requires main to return int. */
 }
