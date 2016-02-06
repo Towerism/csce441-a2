47c0c6c7339a43252b0e0d587b268dc7f1b13d93
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-06 11:37:23 -0600

    Remove unneeded static function

--- tween.hh
+++ tween.hh
@@ -3,19 +3,6 @@
 class Tween {
 public:
   // Tweens a current value to a target value using increment over time
-  // when positive is true, the value will not be allowed to fall below 0.0
-  static void linear(float& current, float target, float increment, bool positive) {
-    if (current < target)
-      current -= increment;
-    else if (current > target)
-      current += increment;
-    if (positive && current < 0.0)
-      current = 0.0;
-  }
-
-  Tween() : complete(false) {}
-
-  // Tweens a current value to a target value using increment over time
   // Once the tween is complete, current is set to 0
   void oneShotLinear(float& current, float target, float increment) {
     this->current = current;
