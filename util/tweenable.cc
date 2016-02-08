671f5421d639630e411db32ccbee6d4ea68b3434
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-07 23:34:53 -0600

    Get rid of cout statements

--- util/tweenable.cc
+++ util/tweenable.cc
@@ -1,18 +1,11 @@
 #include "tweenable.hh"
 
-#include <iostream>
 void Tweenable::tween(float delta, float increment) {
   float biasAdjustedDelta = delta / bias;
   tweener.oneShotLinear(value, base + biasAdjustedDelta, increment);
-  std::cout << "{before floor" << std::endl;
-  std::cout << "value: " << value << std::endl;
-  std::cout << "}" << std::endl;
   if (value < minimum) {
     value = minimum;
   }
-  std::cout << "{after floor" << std::endl;
-  std::cout << "value: " << value << std::endl;
-  std::cout << "}" << std::endl;
 }
 
 void Tweenable::reset() {
